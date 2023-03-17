69f6c433974e3d1d531e94ea112170a1150c4dd7
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2023-03-17 15:46:15 +0100

    zad2 skonczone

--- lab3/JedrzejewskiFilip/cw03/zad2/main.c
+++ lab3/JedrzejewskiFilip/cw03/zad2/main.c
@@ -7,11 +7,11 @@
 int main(int argn, char* argv[]){
     //pobieram dane wejsciowe i sprawdzam czy wszystko z nimi ok
     if(argn == 2){
+        char* name = argv[0];
         char* path = argv[1];
 
         //print nazwy programu
-        //TODO
-        printf("XD ");
+        printf("Nazwa programu: %s, pliki we wskazanym katalogu: ", name);
 
         //wymuszam brak buforowania danych
         setbuf(stdout, NULL);
