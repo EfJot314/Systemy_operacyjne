fc1a5a8fb2a1411c74777e64cda242e1e69cf136
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2023-03-28 19:26:36 +0200

    sygnaly zad3-zrobione

--- lab4/JedrzejewskiFilip/cw04/zad3/catcher.c
+++ lab4/JedrzejewskiFilip/cw04/zad3/catcher.c
@@ -4,27 +4,114 @@
 #include<unistd.h>
 #include<string.h>
 #include<stdlib.h>
+#include<time.h>
 
-
+//zmienna obslugujaca nieskonczona petle utrzymujaca program przy zyciu
 int stoper = 1;
+//zmienna przechowujaca PID procesu wyspisujacego czas
+int timePID = 0;
+//zmienna czasu
+time_t t;
+//zmienna przechowujaca liczbe zmian trybu pracy
+int nOfChanges = 0;
 
 
+//funkcja wykonuje dzialania ktore powinny byc wykonywane na poczatku kazdego trybu pracy
+void tribeInit(){
+    //inkrementuje licznik zmian trybu pracy
+    nOfChanges++;
+    //jesli jest wlaczony tryb 4 to go koncze wysylajac sygnal SIGINT (Ctrl+C)
+    if(timePID != 0){
+        kill(timePID, SIGINT);
+        timePID = 0;
+    }
+}
+
+
+//funkcja wysylajaca sygnal SIGUSR1 z wartocia value do procesu o podanym PID
 void sendSignal(int PID, int value){
     union sigval ssv;
     ssv.sival_int = value;
-    sigqueue(PID, SIGUSR1, ssv);
+    //wysylam sygnal i sprawdzam czy nie bylo z tym problemow
+    int result = sigqueue(PID, SIGUSR1, ssv);
+    if(result < 0){
+        perror("Sygnal potwierdzajacy nie zostal wyslany!\n");
+    }
 }
 
+
+//handler sygnalu SIGUSR1
 void handle(int sig_no, siginfo_t *info, void *ucontext){
     int senderPID = info->si_pid;
     union sigval sv = info->si_value;
+    
+    //odczytanie trybu
+    int tribe = sv.sival_int;
 
-    printf("Otrzymano sygnal!\n");
+    //wykonuje zadania okreslone dla kazdego trybu pracy
+
+    //1 - wypisanie liczb od 1 do 100
+    if(tribe == 1){
+        tribeInit();
+
+        printf("Liczby od 1 do 100: ");
+        for(int i=1;i<=100;i++){
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+    //2 - wypisanie aktualnego czasu
+    else if(tribe == 2){
+        tribeInit();
+
+        time(&t);
+        printf("Aktualny czas: %s", ctime(&t));
+    }
+    //3 - wypisanie liczby zmian trybu pracy
+    else if(tribe == 3){
+        tribeInit();
+
+        printf("Liczba zmian trybu pracy: %d\n", nOfChanges);
+    }
+    //4 - wypisywanie aktualnego czasu co 1 s
+    else if(tribe == 4){
+        nOfChanges++;
+        //jesli ten tryb juz pracuje to nic nie robie
+        if(timePID == 0){
+            //tworze nowy proces dla wypisywania czasu
+            int newPID = fork();
+            if(newPID == 0){
+                //nieskonczona petla wypisujaca czas co 1 s dopoki nie zmienie trybu
+                while(1){
+                    time(&t);
+                    printf("Aktualny czas: %s", ctime(&t));
+                    sleep(1);
+                }
+            }
+            else{
+                timePID = newPID;
+            }
+        }
+        
+        
+    }
+    //5 - koniec pracy programu catcher
+    else if(tribe == 5){
+        tribeInit();
+
+        //po prostu wychodze z nieskonczonej petli
+        stoper = 0;
+    }
+    //jesli nie wybrano zadnego trybu
+    else{
+        perror("Nie rozpoznano trybu!\n");
+    }
     
     //wysylam potwierdzenie
     sendSignal(senderPID, 0);
     
 }
+
 
 
 int main(){
@@ -41,10 +128,9 @@ int main(){
     sigaction(SIGUSR1, &act, NULL);
 
 
-
     //czekam na sygnaly
     while(stoper){
-
+        
     }
 
 
--- lab4/JedrzejewskiFilip/cw04/zad3/sender.c
+++ lab4/JedrzejewskiFilip/cw04/zad3/sender.c
@@ -6,16 +6,26 @@
 #include<stdlib.h>
 
 
+//deklaracje zmiennych
 int catcherPID;
 int stoper = 1;
 
 
+//funkcja wysylajaca sygnal SIGUSR1 z wartocia value do procesu o podanym PID
 void sendSignal(int PID, int value){
     union sigval ssv;
-    ssv.sival_int = 1;
-    sigqueue(PID, SIGUSR1, ssv);
+    ssv.sival_int = value;
+    //wysylam sygnal i sprawdzam czy nie bylo z tym problemow
+    int result = sigqueue(PID, SIGUSR1, ssv);
+    if(result < 0){
+        perror("Sygnal nie zostal wyslany!\n");
+        //zeruje stoper, poniewaz chce przejsc do kolejnego polecenia
+        stoper = 0;
+    }
 }
 
+
+//handler sygnalu SIGUSR1
 void handle(int sig_no, siginfo_t *info, void *ucontext){
     int senderPID = info->si_pid;
     union sigval sv = info->si_value;
@@ -25,7 +35,6 @@ void handle(int sig_no, siginfo_t *info, void *ucontext){
         if(sv.sival_int == 0){
             //to pozwalam wyslac kolejny sygnal
             stoper = 0;
-            printf("Fajnie, wrocilo!\n");
         }
         else{
             perror("Nie otrzymano potwierdzenia wykonania polecenia!\n");
@@ -38,9 +47,10 @@ void handle(int sig_no, siginfo_t *info, void *ucontext){
 }
 
 
+
 int main(int argc, char* argv[]){
     //pobieram argumenty i sprawdzam czy wszystko z nimi jest ok
-    if(argc == 2){
+    if(argc >= 3){
         //pobieram PID i sprawdzam czy jest ok
         catcherPID = atoi(argv[1]);
         if(catcherPID == 0){
@@ -57,11 +67,30 @@ int main(int argc, char* argv[]){
         sigaction(SIGUSR1, &act, NULL);
 
 
-        //wysylam sygnal
-        sendSignal(catcherPID, 1);
+        
+        //przechodze po argumentach i wysylam polecenia do catchera
+        for(int i=2;i<argc;i++){
+            //zamieniam argument na liczbe i sprawdzam czy wszystko jest ok
+            int tribeNumber = atoi(argv[i]);
+            if(tribeNumber == 0){
+                perror("Niepoprawny podany tryb pracy");
+                exit(4);
+            }
 
-        while(stoper){
+            //jesli jest ok, to wysylam polecenie do catchera
+            sendSignal(catcherPID, tribeNumber);
             
+            //jesli poleceniem byl tryb 5, to koncze prace sendera
+            if(tribeNumber == 5){
+                return 0;
+            }
+
+            //zatrzymuje dzialanie programu dopoki nie dostane potwierdzenia wykonania polecenia
+            while(stoper){
+
+            }
+            stoper = 1;
+
         }
         
 
@@ -70,7 +99,6 @@ int main(int argc, char* argv[]){
         perror("Niepoprawna liczba argumentow!\n");
         exit(1);
     }
-
 
 
     return 0;
