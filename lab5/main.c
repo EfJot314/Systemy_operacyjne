23cf94fe94235d9d3cbe8c035f60b0c434ffa57a
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2023-03-30 11:01:39 +0200

    koniec laboratoryjnej zabawy

--- lab5/main.c
+++ lab5/main.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include <fcntl.h>
+
 
 
 
@@ -10,51 +12,21 @@
 
 int main(){
 
-    //tworze pipe1
-    int fd1[2];
-    pipe(fd1);
+    //otwieram plik
+    int file = open("./out.txt", O_RDONLY);
 
-    //tworze pipe2
-    int fd2[2];
-    pipe(fd2);
+    //przekierowuje input z stdin na plik
+    dup2(file, 0);
 
-    //tworze dziecko
-    int newPID = fork();
+    int wynik;
+    scanf("%d", &wynik);
 
-    //jesli dziecko
-    if(newPID == 0){
-        //zamykam nieuzywany koniec do pisania
-        close(fd1[1]);
-        //zamykam nieuzywany koniec do czytania
-        close(fd2[0]);
-        //tworzenie buforu
-        char buf[100];
-        //czytam do bufru z pipa
-        read(fd1[0], buf, 100);
-        //drukuje bufor
-        printf("%s\n", buf);
-        //modyfikuje bufor
-        strcat(buf, " world!");
-        //wysylam bufor
-        write(fd2[1], buf, 100);
-    }
-    //jesli rodzic
-    else{
-        //zamykam nieuzywana koncowke do czytania
-        close(fd1[0]);
-        //zamykam nieuzywany koniec do pisania
-        close(fd2[1]);
-        //wysylam wiadomosc
-        write(fd1[1], "Hello", 100);
-        //czytam zwrot
-        char buf[100];
-        read(fd2[0], buf, 100);
-        //printuje
-        printf("%s\n", buf);
-
-    }
+    //zamykam plik
+    close(file);
 
 
+    printf("%d\n", wynik);
 
+    return 0;
 
 }
