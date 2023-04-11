0b3931f1b941945ae9598821734a860f6c0fa8b1
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2023-04-11 20:45:40 +0200

    lab5-opornie, ale jakos to idzie

--- lab5/JedrzejewskiFilip/cw05/zad3/helper.c
+++ lab5/JedrzejewskiFilip/cw05/zad3/helper.c
@@ -36,6 +36,9 @@ int main(int argc, char* argv[]){
             xp += h;
         }
 
+
+        
+
         
 
 
--- lab5/JedrzejewskiFilip/cw05/zad3/main.c
+++ lab5/JedrzejewskiFilip/cw05/zad3/main.c
@@ -2,13 +2,63 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<time.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
 
 
 
 int main(int argc, char* argv[]){
 
-    
+    //sprawdzam czy liczba argumentow sie zgadza
+    if(argc == 3){
+        //zbieram dane z wejscia i sprawdzam czy sa poprawne
+        double h = strtod(argv[1], NULL);
+        if(h == 0){
+            perror("Niepoprany pierwszy argument!");
+            exit(1);
+        }
+        int n = atoi(argv[2]);
+        if(n == 0){
+            perror("Niepoprany drugi argument!");
+            exit(2);
+        }
 
+        //granice przedzialu
+        double x_min = 0;
+        double x_max = 1;
+
+        //obliczam szerokosc przedzialu ktory otrzyma do obliczenia kazdy z procesow potomnych
+        double dx = (x_max - x_min) / n;
+
+        //wlaczam zegar
+        // startTime();
+
+        //tworze potok nazwany
+        const char* path = "potoczek";
+        //S_IRWXU - czytanie, pisanie, wykonywanie
+        int kkk = mkfifo(path, S_IRWXU);
+
+        for(int i=0;i<n;i++){
+            int res = open(path, O_RDONLY);
+            if(res > 0){
+                printf("sup\n");
+            }
+            else{
+                printf("ups\n");
+            }
+        }
+
+        //usuwam potok
+        remove(path);
+
+        //czas stop
+        // stopTime();
+
+    }
+    else{
+        perror("Niepoprawna liczba argumentow!");
+    }
 
 
     return 0;
