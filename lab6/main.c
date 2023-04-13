2410fca9ffa6fdd139113b08cb3e97811118aaf0
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2023-04-13 10:24:25 +0200

    lab6- jak krew z nosa

--- lab6/main.c
+++ lab6/main.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
-
+#include <unistd.h>
+#include <limits.h>
+#include "sys/msg.h"
 
 
 
@@ -8,8 +10,15 @@
 
 
 int main(){
+    //tworze klucz
+    int key = ftok(".", 'F');
 
-    printf("HW\n");
+    int res = msgget(key, IPC_CREAT | 0666);
+    if(res == -1){
+        perror("Blad podczas tworzenia kolejki!");
+        exit(1);
+    }
+
 
     return 0;
 }
