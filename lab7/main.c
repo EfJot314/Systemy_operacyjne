d00b2cce83d6a258b8f39c2ea5374ef08e90fbe1
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2023-04-27 10:59:47 +0200

    lab7-ciemno to widze

--- lab7/main.c
+++ lab7/main.c
@@ -11,24 +11,15 @@ int main(){
 
     int sems = semget(key, 1, IPC_CREAT | 0666);
 
-    struct sembuf sss;
-    sss.sem_num = 0;
-    sss.sem_num = -1;
-    sss.sem_flg = IPC_NOWAIT;
 
-    
 
-    semop(sems, &sss, 1);
+    int newPID = fork();
+    if(newPID == 0){
+        
+    }
+    else{
 
-    semctl(sems, 0, SETVAL, 1);
-
-    union semum cc;
-
-    semctl(sems, 0, GETVAL, cc);
-
-    printf("%d\n", cc.val);
-
-    semctl(sems, 0, IPC_RMID);
+    }
 
 
     return 0;
