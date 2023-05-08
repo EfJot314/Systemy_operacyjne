9fd66b8ad644b909b93eb194ccfad44e6e3b054f
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2023-05-08 23:00:38 +0200

    lab8-zad1 - nwm czm ale udalo mi sie usunac  jednego grida

--- lab8/JedrzejewskiFilip/zad2/grid.h
+++ lab8/JedrzejewskiFilip/zad2/grid.h
@@ -6,6 +6,6 @@ char *create_grid();
 void destroy_grid(char *grid);
 void destroy_threads(pthread_t** threads);
 void draw_grid(char *grid);
-pthread_t** init_grid(char *grid);
+pthread_t** init_grid(char *grid1, char *grid2);
 bool is_alive(int row, int col, char *grid);
 void update_grid(pthread_t** threads);
