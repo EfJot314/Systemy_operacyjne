3ea11d77254f5a08ebce808815e15cec046c62a2
EfJot314 <62033206+EfJot314@users.noreply.github.com>
2023-05-16 23:06:01 +0200

    lab9-koniec

--- lab9/JedrzejewskiFilip/cw09/zad1/main.c
+++ lab9/JedrzejewskiFilip/cw09/zad1/main.c
@@ -7,12 +7,15 @@
 #include <unistd.h>
 
 
+
+
 //kolory
 #define MY_COLOR_RED     "\x1b[31m"
 #define MY_COLOR_GREEN   "\x1b[32m"
 #define MY_COLOR_YELLOW  "\x1b[33m"
 #define MY_COLOR_RESET   "\x1b[0m"
 
+//stale
 #define nElf 10
 #define nRen 9
 
@@ -28,22 +31,26 @@ pthread_cond_t elfWaitCond = PTHREAD_COND_INITIALIZER;
 pthread_cond_t reniferCond = PTHREAD_COND_INITIALIZER;
 pthread_cond_t mikolajCond = PTHREAD_COND_INITIALIZER;
 
+
 //zmienne wspoldzielone
 int waitingElfs = 0;
 int elfID[3];
 int waitingRenifers = 0;
 
 
-int flag = 1;
+//zmienna odpowiadajaca za zycie Mikolaja
+int years = 0;
+
 
 void handler(int signum){
-    //koncze dzialanie programu -> przerywam petle
-    flag = 0;
+    //koncze dzialanie programu -> przerywam petle -> przyspieszam czas
+    years = 3;
 }
 
 
+//funkcja zwraca losowa liczbe typu int z przedzialu <a,b>
 int randint(int a, int b){
-    return a + rand() % (b-a);
+    return a + rand() % (b-a+1);
 }
 
 
@@ -62,30 +69,25 @@ void* renifer(void* args){
         pthread_mutex_lock(&mikolajMutex);
         waitingRenifers++;
         printf(MY_COLOR_YELLOW "Renifer: czeka %d reniferów na Mikołaja, %d\n" MY_COLOR_RESET, waitingRenifers, id);
-        //czekam na Mikolaja
-        if(waitingRenifers < 9){
-            while(waitingRenifers < 9){
-                pthread_cond_wait(&reniferCond, &mikolajMutex);
-            }
-        }
-        else{
-            //krzycze do pozostalych ze czas sie zbierac
-            pthread_cond_broadcast(&reniferCond);
 
+        //czekam na Mikolaja
+        if(waitingRenifers == 9){
             //jesli juz sa wszyscy to budzimy Mikolaja
             if(waitingRenifers == 9){
                 //budze Mikolaja
                 printf(MY_COLOR_YELLOW "Renifer: wybudzam Mikołaja, %d\n" MY_COLOR_RESET, id);
+
                 pthread_cond_broadcast(&mikolajCond);
             }
         }
-        
+
         //czekamy na dowiezienie wszystkich prezentow
         while(waitingRenifers != 0){
             pthread_cond_wait(&reniferCond, &mikolajMutex);
         }
 
         pthread_mutex_unlock(&mikolajMutex);
+        
 
     }
 }
@@ -95,6 +97,7 @@ void* elf(void* args){
     //zbieram id z wejscia
     int id = *(int*)args;
     free((int*)args);
+
     //glowna petla elfa
     while(1){
         //najpierw praca
@@ -102,39 +105,46 @@ void* elf(void* args){
 
         //potem problemy
         pthread_mutex_lock(&mikolajMutex);
-        //jesli problemy aktualnie sa rozwiazywane to nie zglaszam zadnych
-        while(waitingElfs == -1){
-            pthread_cond_broadcast(&elfCond);
-        }
 
         //jesli jednak mikolaj spi albo jezdzi sobie z reniferami po swiecie to zglaszam swoje problemy
         int waiting = 0;
         int myInd = 0;
+
+        //lece do Mikolaja
         if(waitingElfs < 3){
             pthread_mutex_lock(&elfMutex);
             elfID[waitingElfs] = id;
             pthread_mutex_unlock(&elfMutex);
+
             waitingElfs++;
             printf(MY_COLOR_GREEN "Elf: czeka %d elfów na Mikołaja, %d\n" MY_COLOR_RESET, waitingElfs, id);
+
             if(waitingElfs == 3){
                 printf(MY_COLOR_GREEN "Elf: wybudzam Mikołaja, %d\n" MY_COLOR_RESET, id);
                 pthread_cond_broadcast(&mikolajCond);
             }
+
             waiting = 1;
             myInd = waitingElfs-1;
         }
+        //jakos sam sobie musze poradzic
         else{
             printf(MY_COLOR_GREEN "Elf: samodzielnie rozwiązuję swój problem, %d\n" MY_COLOR_RESET, id);
         }
-        pthread_mutex_unlock(&mikolajMutex);
 
+        pthread_mutex_unlock(&mikolajMutex);
+        
+        //jesli musze czekac to czekam
         if(waiting){
             //czekam na rozwiazanie moich problemow
             pthread_mutex_unlock(&elfMutex);
+
             while(elfID[myInd] == id){
                 pthread_cond_wait(&elfWaitCond, &elfMutex);
             }
+
             printf(MY_COLOR_GREEN "Elf: Mikołaj rozwiązuje problem, %d\n" MY_COLOR_RESET, id);
+
             pthread_mutex_unlock(&elfMutex);
             
         }
@@ -142,6 +152,59 @@ void* elf(void* args){
     }
 }
 
+
+void* mikolaj(){
+    while(1){
+        pthread_mutex_lock(&mikolajMutex);
+
+        //spie
+        printf(MY_COLOR_RED "Mikołaj: zasypiam\n" MY_COLOR_RESET);
+        while(waitingElfs != 3 && waitingRenifers < 9){
+            pthread_cond_wait(&mikolajCond, &mikolajMutex);
+        }
+
+        //budze sie
+        printf(MY_COLOR_RED "Mikołaj: budzę się\n" MY_COLOR_RESET);
+
+        //obsluga elfow
+        if(waitingElfs == 3){
+            printf(MY_COLOR_RED "Mikołaj: rozwiązuje problemy elfów %d, %d, %d \n" MY_COLOR_RESET, elfID[0], elfID[1], elfID[2]);
+            
+            for(int i=0;i<3;i++){
+                pthread_mutex_lock(&elfMutex);
+
+                elfID[i] = -1;
+                pthread_cond_broadcast(&elfWaitCond);
+
+                pthread_mutex_unlock(&elfMutex);
+
+                //czekam
+                sleep(randint(1,2));
+                
+            }
+            
+            waitingElfs = 0;
+            pthread_cond_broadcast(&elfCond);
+        }
+
+        //obsluga reniferow
+        if(waitingRenifers == 9){
+            //dostarczam zabawki
+            printf(MY_COLOR_RED "Mikołaj: dostarczam zabawki\n" MY_COLOR_RESET);
+
+            sleep(randint(2,4));
+
+            years++;
+            waitingRenifers = 0;
+
+            pthread_cond_broadcast(&reniferCond);
+        }
+
+        pthread_mutex_unlock(&mikolajMutex);
+
+         
+    }
+}
 
 
 int main(){
@@ -178,51 +241,20 @@ int main(){
     }
 
 
+    //tworze Mikolaja
+    pthread_t* mikolajT = (pthread_t*)malloc(sizeof(pthread_t));
+    pthread_create(mikolajT, NULL, &mikolaj, NULL);
+
+
+
     //przechwytuje ctr+c
     signal(SIGINT, handler);
 
 
-    //nieskonczona petla bedaca Mikolajem
-    while(flag){
-        pthread_mutex_lock(&mikolajMutex);
-        //spie
-        while(waitingElfs != 3 && waitingRenifers < 9){
-            pthread_cond_wait(&mikolajCond, &mikolajMutex);
-        }
-        //budze sie
-        printf(MY_COLOR_RED "Mikołaj: budzę się\n" MY_COLOR_RESET);
-        //obsluga elfow
-        if(waitingElfs == 3){
-            printf(MY_COLOR_RED "Mikołaj: rozwiązuje problemy elfów %d, %d, %d \n" MY_COLOR_RESET, elfID[0], elfID[1], elfID[2]);
-            //ustawiam tak, aby elfy nie mialy chwilowo problemow
-            waitingElfs = -1;
-            // pthread_mutex_lock(&elfMutex);
-            for(int i=0;i<3;i++){
-                pthread_mutex_lock(&elfMutex);
-                elfID[i] = -1;
-                pthread_cond_broadcast(&elfWaitCond);
-                pthread_mutex_unlock(&elfMutex);
 
-                sleep(randint(1,2));
-                
-            }
-            
-            waitingElfs = 0;
-            pthread_cond_broadcast(&elfCond);
-        }
-        //obsluga reniferow
-        if(waitingRenifers == 9){
-            //dostarczam zabawki
-            printf(MY_COLOR_RED "Mikołaj: dostarczam zabawki\n" MY_COLOR_RESET);
-            sleep(randint(2,4));
-            waitingRenifers = 0;
-            pthread_cond_broadcast(&reniferCond);
-        }
-        
+    //petla blokujaca zakonczenie programu do czasu spelnienia warunku 3 wyjazdow Mikolaja
+    while(years < 3){}
 
-        pthread_mutex_unlock(&mikolajMutex);
-        
-    }
 
 
     //zabijam renifery
@@ -239,9 +271,9 @@ int main(){
     }
     free(elfs);
 
-    //zabijam mikolaja
-    // pthread_cancel(*mikolajT);
-    // free(mikolajT);
+    //zabijam Mikolaja
+    pthread_cancel(*mikolajT);
+    free(mikolajT);
 
     return 0;
 }
